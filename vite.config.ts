f8392a23d7e5e6e0f3025f06f8ccac1c88144c6c
Elena Volpato <elena.volpato@digi-sapiens.de>
2025-10-14 15:55:37 +0200

    test of base yml file

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import devtools from "solid-devtools/vite";
 import solid from "vite-plugin-solid";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [devtools(), solidPlugin(), solid()],
+  plugins: [devtools(), solidPlugin(), solid(), tailwindcss()],
+
+  base: "/",
   server: {
     port: 3000,
   },
