96923b1db8c5ed8a065c4b8d00f21eefc9c27357
Elena Volpato <elena.volpato@digi-sapiens.de>
2025-10-14 10:33:02 +0200

    updates vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-import devtools from 'solid-devtools/vite';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import devtools from "solid-devtools/vite";
+import solid from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [devtools(), solidPlugin()],
+  plugins: [devtools(), solidPlugin(), solid()],
   server: {
     port: 3000,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
 });
